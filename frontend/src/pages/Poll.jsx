808f9ea1bb88fa931af4a84c9815149270dfc284
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-12 18:42:25 +0300

    poll.jsx added

---
+++ frontend/src/pages/Poll.jsx
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import api from '../services/api';
+
+function Poll() {
+  const { id } = useParams();
+  const [poll, setPoll] = useState(null);
+  const [selected, setSelected] = useState(null);
+  const [voted, setVoted] = useState(false);
+
+  useEffect(() => {
+    api.get(`/polls/${id}`)
+      .then(res => setPoll(res.data))
+      .catch(err => console.error(err));
+  }, [id]);
+
+  const submitVote = () => {
+    api.post(`/polls/${id}/vote`, { option_id: selected })
+      .then(() => setVoted(true))
+      .catch(err => alert('Vote failed'));
+  };
+
+  if (!poll) return <div>Loading...</div>;
+
+  return (
+    <div>
+      <h2>{poll.question}</h2>
+      {poll.options.map(opt => (
+        <div key={opt.id}>
+          <label>
+            <input
+              type="radio"
+              name="vote"
+              value={opt.id}
+              onChange={() => setSelected(opt.id)}
+            />
+            {opt.text}
+          </label>
+        </div>
+      ))}
+      <button onClick={submitVote} disabled={!selected || voted}>
+        {voted ? 'Thanks for Voting!' : 'Submit Vote'}
+      </button>
+
+      {voted && (
+        <div>
+          <h3>Results:</h3>
+          <ul>
+            {poll.options.map(opt => (
+              <li key={opt.id}>
+                {opt.text}: {opt.vote_count} votes
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Poll;
