68cdd9f5ff17fac46ec2604bce128b70943bd9a9
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-12 14:40:58 +0300

    model.py modified

---
+++ backend/models.py
@@ -0,0 +1,16 @@
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+db = SQLAlchemy()
+
+class Poll(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    options = db.relationship('Option', backref='poll', cascade="all, delete", lazy=True)
+
+class Option(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(100), nullable=False)
+    votes = db.Column(db.Integer, default=0)
+    poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'), nullable=False)
