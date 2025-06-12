e1e89dafb428b6f74544f280926b2d8ac63d2fa6
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-12 18:38:51 +0300

    app.jsx added

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CreatePoll from './pages/CreatePoll';
+import Poll from './pages/Poll';
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <h1>🗳 Voting App</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePoll />} />
+          <Route path="/polls/:id" element={<Poll />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
