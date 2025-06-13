38c20f710f18a36ff7b60bdc652bbff491dd0e35
kipngenohaaron <kipngenohaaron@gmail.com>
2025-06-13 10:54:16 +0300

    main.jsx added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,13 +6,22 @@ import Poll from './pages/Poll';
 function App() {
   return (
     <Router>
-      <div className="container">
-        <h1>🗳 Voting App</h1>
-        <Routes>
+      <div className="container py-4">
+  <h1 className="mb-4 text-center">🗳 Voting App</h1>
+   <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/create" element={<CreatePoll />} />
           <Route path="/polls/:id" element={<Poll />} />
         </Routes>
+</div>
+
+      <div className="container">
+        <h1>🗳 Voting App</h1>
+        {/* <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePoll />} />
+          <Route path="/polls/:id" element={<Poll />} />
+        </Routes> */}
       </div>
     </Router>
   );
